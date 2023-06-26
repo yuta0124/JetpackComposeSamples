d881c03a69028268e4458ed404b9cbec38f59908
satoyu19 <21cm0119@jec.ac.jp>
2023-06-26 22:45:43 +0900

    change:file

--- ProfileCard/app/src/main/java/com/example/profilecard/MainActivity.kt
+++ ProfileCard/app/src/main/java/com/example/profilecard/MainActivity.kt
@@ -3,13 +3,29 @@ package com.example.profilecard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.profilecard.ui.theme.ProfileCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CreateBizCard()
                 }
             }
         }
@@ -30,17 +46,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CreateBizCard() {
+    Surface(modifier = Modifier.fillMaxSize()) { //fillMaxHeight * fillMaxWidth = fillMaxSize
+        Card(
+            modifier = Modifier
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            ),
+            elevation = CardDefaults.cardElevation(5.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .height(300.dp)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // NOTE: 恐らく影をつけるためにSurfaceで囲ってるが、ImageがSurfaceと同じサイズにならない。
+                Surface(
+                    modifier = Modifier
+                        .size(150.dp)
+                        .padding(5.dp),
+                    shape = CircleShape,    // 丸型
+                    shadowElevation = 4.dp,  // サーフェス下の影のサイズ
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+                ) {
+                    Image(
+                        modifier = Modifier.size(135.dp),    // 削除しても変わらない
+                        painter = painterResource(id = R.drawable.account),
+                        contentDescription = "ProfileImage",
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ProfileCardTheme {
-        Greeting("Android")
+        CreateBizCard()
     }
 }
